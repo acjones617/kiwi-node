20c9755df1b6c47ce35f3eb0582b2c29a9e9f936
Ramin Taleghani <talegram@gmail.com>
2014-05-22 23:41:30 -0700

    adds dependencies for nvd3 library

--- app/scripts/controllers/kiwis.js
+++ app/scripts/controllers/kiwis.js
@@ -5,10 +5,20 @@ angular.module('kiwiNode2App')
     //TODO: revisit the url
     $http({
       method: 'GET',
-      url: 'api/kiwis/andrewk0291@gmail.com'
+      url: 'api/kiwis/' + $routeParams.email
     })
     .then(function(data) {
-      $scope.kiwis = data.data;
+      data = data.data;
+      for (var i = 0; i < data.length; i++) {
+        data[i].graphData = {
+          key: 'series' + i,
+          values: [] 
+        };
+        for (var j in data[i].values) {
+          data[i].graphData.values.push([j, data[i].values[j]]);
+        }
+      }
+      $scope.kiwis = data;
     })
     .catch(function() {
       $scope.errors.other = 'Error with retrieving kiwis.';

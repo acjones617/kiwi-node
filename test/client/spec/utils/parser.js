c3a34797a6902462b6c56db09fbaa55037949c7c
Sean Dokko <dokko1230@gmail.com>
2014-05-25 22:33:01 -0700

    adds tests for parser

---
+++ test/client/spec/utils/parser.js
@@ -0,0 +1,17 @@
+var _ = require('underscore');
+var expect = require('chai').expect;
+var Utils = {};
+var ValueParser = require('../../../../app/scripts/lib/ValueParser');
+
+describe('value parser', function () {
+  it('exists', function () {
+    expect(ValueParser).to.exist;
+    expect(ValueParser).to.be.a('function');
+  });
+
+  it('parses the value with a naive algorithm', function () {
+    var original = "$585.64";
+    var rest = ["$585.64$581.35", "$585.66$582.25", "$586.03$583.38"];
+    var parser = new ValueParser(original, rest);
+  });
+});

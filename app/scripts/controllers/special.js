68e243ff193debe3b9fe2431e0a8c0dad10be2bc
Sean Dokko <dokko1230@gmail.com>
2014-05-29 12:48:12 -0700

    fixes the special popup view

--- app/scripts/controllers/special.js
+++ app/scripts/controllers/special.js
@@ -1,29 +1,32 @@
 'use strict';
 
-angular.module('kiwiNode2App')
+angular.module('KiwiApp')
   .controller('SpecialCtrl', function ($scope, $cookies, $firebase, $firebaseSimpleLogin) {
     
     var ref = new Firebase('https://kiwidb.firebaseio.com/');
 
 
 
-    $scope.doStuff = function() {
-   var auth = new FirebaseSimpleLogin(ref, function(err, user) {
-      if (err) {
-        console.log('Error:, ', err);
-      } else {
-        if (user) {
-          // note: not using angular cookies as they are not working
-          // $cookies.kiwiSpecial = user.firebaseAuthToken;
-          // $cookies.kiwiUid = user.uid;
-          // TODO: fix this
-          document.cookie = 'kiwiSpecial='+user.firebaseAuthToken+'; expires=Fri, 3 Aug 2014 20:47:11 UTC; path=/';
-          document.cookie = 'kiwiUid='+user.uid+'; expires=Fri, 3 Aug 2014 20:47:11 UTC; path=/';
-          open(location, '_self').close(); // TODO:
-        }
-      }
-    });
-      auth.login('facebook');
+  $scope.doStuff = function() {
+    var auth = new FirebaseSimpleLogin(ref, function(err, user) {
+       if (err) {
+         console.log('Error:, ', err);
+       } else {
+         if (user) {
+           // note: not using angular cookies as they are not working
+           // $cookies.kiwiSpecial = user.firebaseAuthToken;
+           // $cookies.kiwiUid = user.uid;
+           // TODO: fix this
+           document.cookie = 'kiwiSpecial='+user.firebaseAuthToken+'; expires=Fri, 3 Aug 2014 20:47:11 UTC; path=/';
+           document.cookie = 'kiwiUid='+user.uid+'; expires=Fri, 3 Aug 2014 20:47:11 UTC; path=/';
+           $('body').append('<h1>You have been successfully logged in</h1>');
+           setTimeout(function(){
+            open(location, '_self').close(); // TODO:
+           }, 5000); 
+         }
+       }
+     });
+       auth.login('facebook');
     };
 
 

f70b5b1288803286ecf8e7cf0b22cf55c3eb12bd
Rohan <sarith21@gmail.com>
2014-05-28 18:07:49 -0700

    promisifies login function so as to send message to server on completion

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('KiwiApp')
-  .controller('NavbarCtrl', function ($scope, $location, $firebase, $firebaseSimpleLogin) {
+  .controller('NavbarCtrl', function ($scope, $location, $firebase, $firebaseSimpleLogin, $q) {
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
@@ -19,8 +19,22 @@ angular.module('KiwiApp')
     
     var ref = new Firebase('https://kiwidb.firebaseio.com/');
     $scope.auth = $firebaseSimpleLogin(ref);
-     
+    
+    $scope.getCreds = function(){
+      $scope.prms = $scope.authRegister();
+      $scope.prms.then(function(result){
+        console.log(result);
+      })
+    }
+
+    $scope.authRegister = function(){
+      var deferred = $q.defer();
+      deferred.resolve( $scope.auth.$login('facebook') );
+      return deferred.promise;
+    }
+
     $scope.isActive = function(route) {
       return route === $location.path();
     };
   });
+

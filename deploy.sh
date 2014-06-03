6d4ddb1c216a3ba877c7a9120175281fb0bfcf6d
Sean Dokko <dokko1230@gmail.com>
2014-06-02 20:59:41 -0700

    adds -g to grunt-cli

--- deploy.sh
+++ deploy.sh
@@ -133,7 +133,7 @@ echo "Finished installing bower packages"
 # 5. Run grunt
 if [ -e "$DEPLOYMENT_TARGET/Gruntfile.js" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install grunt-cli
+  eval $NPM_CMD install grunt-cli -g
   exitWithMessageOnError "installing grunt failed"
   ./node_modules/.bin/grunt --no-color build
   exitWithMessageOnError "grunt failed"
